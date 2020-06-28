27ad89f585141cd5945fa97fa42d147dfb648403
houhanzhi <18511751149@163.com>
2020-06-28 10:31:23 +0800

    week06

---
+++ Week_06/CountSubstrings.java
@@ -0,0 +1,22 @@
+package com.datastructure.algorithm.week06;
+
+/**
+ * 回文子串
+ */
+public class CountSubstrings {
+
+    public int countSubstrings(String s) {
+        int N = s.length(), ans = 0;
+        for (int center = 0; center <= 2*N-1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < N && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+
+}
